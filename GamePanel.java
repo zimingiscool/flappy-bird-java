cc62ad5941d6019330353bf6cbb8cb9fa552ea45
zo <65860568+zimingiscool@users.noreply.github.com>
2023-01-20 21:07:05 -0500

    Add Files
    Create New Classes

---
+++ GamePanel.java
@@ -0,0 +1,17 @@
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+    GamePanel(){
+        
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
